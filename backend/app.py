539fecaa2d7489453302d519b64ea0eb491fa645
Adrian Porsea Victor Hutauruk Eddy <ae2422@nyu.edu>
2025-06-11 13:13:03 -0400

    Removed security check from app.py

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,8 @@ ALLOWED_ORIGIN = "https://halyconer.github.io/Welcome-to-my-Portfolio/"
 app = Flask(__name__)
 
 # Only allow CORS from your GitHub Pages domain on the /set_brightness route:
-CORS(app, resources={r"/set_brightness": {"origins": ALLOWED_ORIGIN}})
+# CORS(app, resources={r"/set_brightness": {"origins": ALLOWED_ORIGIN}})
+CORS(app)
 
 def get_bulb():
     try:
@@ -18,14 +19,14 @@ def get_bulb():
     except Exception:
         return None
 
-@app.before_request
-def check_auth():
-    if request.endpoint != 'set_brightness':
-        return  
-    if request.headers.get(
-        'q7x!K3L9fN*zaP2vR1yW0hT8mB5sG4c'
-    ) != API_KEY:
-        return jsonify({"error": "Unauthorized"}), 401
+# @app.before_request
+# def check_auth():
+#     if request.endpoint != 'set_brightness':
+#         return  
+#     if request.headers.get(
+#         'q7x!K3L9fN*zaP2vR1yW0hT8mB5sG4c'
+#     ) != API_KEY:
+#         return jsonify({"error": "Unauthorized"}), 401
 
 @app.route('/set_brightness', methods=['POST'])
 def set_brightness():
