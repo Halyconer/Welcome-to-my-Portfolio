d3f7d6fe60d60b3a4e91f697e24eab3ee0d0742c
Adrian Porsea Victor Hutauruk Eddy <ae2422@nyu.edu>
2025-11-16 15:21:00 -0500

    Fixed the Animation

--- script.js
+++ script.js
@@ -1,4 +1,74 @@
 // Portfolio Script
+
+// Typewriter Effect
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 80 - Math.random() * 40; // Faster typing: 40-80ms instead of 100-200ms
+
+    if (this.isDeleting) { delta /= 2; } // Even faster deleting: 20-40ms
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        // If we're at the last text ("Welcome"), stop the animation
+        if (this.loopNum === this.toRotate.length - 1) {
+            // Remove the cursor after a brief pause
+            setTimeout(function() {
+                that.el.innerHTML = '<span class="wrap">' + that.txt + '</span>';
+                that.el.querySelector('.wrap').style.borderRight = 'none';
+            }, 1000);
+            return; // Stop the typewriter completely
+        }
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 300; // Shorter pause between words: 300ms instead of 500ms
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+// Initialize typewriter effect on window load
+window.addEventListener('load', function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    
+    // INJECT CSS for typewriter cursor
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid rgba(13, 59, 102, 0.7); }";
+    document.body.appendChild(css);
+});
+
 const API_BASE_URL = 'https://valid-goblin-full.ngrok-free.app'
 const LIGHTING_API_URL = API_BASE_URL + '/lighting'
 const CONNECT4_API_BASE = API_BASE_URL + '/connect4'
